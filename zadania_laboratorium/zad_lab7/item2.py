bb76734d7013ec43538455b25d1403cfde99d526
apizon813 <01178178@pw.edu.pl>
2023-12-05 10:05:21 +0100

    drugie podejście do item

---
+++ zadania_laboratorium/zad_lab7/item2.py
@@ -0,0 +1,7 @@
+# Zaprojektuj i wykonaj klasy reprezentujące przedmiot o masie podanej w
+# kilogramach oraz pojemnik (który sam jest przedmiotem) o podanym udźwigu.
+# Przedmioty można wkładać i wyjmować z pojemników, o ile są one w ramach
+# dopuszczalnego udźwigu (w przeciwnym razie nie udaje się włożyć przedmiotu do
+# pojemnika). Program ma umożliwić wypisanie zawartości każdego pojemnika w
+# postaci zestawu mas przedmiotów, które się w nim znajdują.
+
