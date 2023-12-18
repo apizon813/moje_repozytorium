34b1fd097401f978c628cf9865bdb874cef3b7bf
apizon813 <01178178@pw.edu.pl>
2023-12-18 10:17:07 +0100

    dalej

--- zadania_film/płacznik/model_io.py
+++ zadania_film/płacznik/model_io.py
@@ -4,8 +4,10 @@ def read_from_file(path):
     # 3. create Person instances based on the data
     # 4. store into a list and return list
     with open(path, 'r') as file_handle:
+        file_handle.readline()
         for line in file_handle:
+            line = line[:-1]
             pass
 
 def  write_to_file(path, people):
-    
+    pass
---
+++ zadania_film/płacznik/test_model_io.py
@@ -0,0 +1,4 @@
+from model_io import read_from_file
+
+def test_read_from_file():
+    read_from_file('people.txt')
