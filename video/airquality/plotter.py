dc3df25888dea12cb04be3c76e56e448f9b29fce
apizon813 <01178178@pw.edu.pl>
2023-12-24 14:59:38 +0100

    added plotter

--- video/airquality/plotter.py
+++ video/airquality/plotter.py
@@ -1,1 +1,17 @@
 import airquality
+import sys
+from random import choice
+
+
+def main(arguments):
+    all_stations = airquality.get_stations()
+    station = choice(all_stations)
+    all_sensors = station.sensors()
+    sensor = choice(all_sensors)
+    readings = sensor.readings()
+    if readings:
+        print(readings[0])
+
+
+if __name__ == '__name__':
+    main(sys.argv)
