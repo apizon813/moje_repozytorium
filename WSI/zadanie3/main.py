0c7efdcf5697e130d8e0bf3bee35100e55fbbf03
apizon <aleksanderpizon@gmail.com>
2024-04-13 01:26:08 +0200

    imlemented who moves logic

--- WSI/zadanie3/main.py
+++ WSI/zadanie3/main.py
@@ -1,15 +1,22 @@
 import numpy as np
 
 
-class Oponent():
+class Player():
     pass
+
+class Oponent():
+    def move():
+        pass
 
 
 class TicTacToe():
-    def __init__(self, dimension: int, who_starts: str):
-        # who_starts = 'p' - player starts
-        # who_starts = 'o' - oponent starts
+    def __init__(self, dimension: int, players, who_starts: str):
+        # who_starts = '0' - player starts
+        # who_starts = '1' - oponent starts
+
         self.dimension = dimension
+        self.players = players
+        self.who_moves = who_starts
         self.board = self.create_board(dimension)
 
     def create_board(self, dimension: int):
@@ -27,16 +34,24 @@ class TicTacToe():
     def print_board():
         pass
 
-    def move():
-        pass
+    def move(self):
+        # do testu
+        self.players[self.who_moves].move(self.board)
+        self.who_moves = not self.who_moves
 
     def finished():
         pass
 
 
 def main():
-    who_starts = input('Who starts? [p/o]\n')
-    game = TicTacToe(3, who_starts)
+    players = (Player(), Oponent())
+    starting_player = int(input('Who starts? [0/1]\n'))
+    game = TicTacToe(
+        dimension=3,
+        players=players,
+        who_starts=starting_player,
+        )
+
     while not game.finished():
         game.move()
     pass
