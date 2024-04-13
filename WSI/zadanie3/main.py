043d41e0abd0e20ab5ec0b2c4bce447f61bbd265
apizon <aleksanderpizon@gmail.com>
2024-04-13 13:37:07 +0200

    implemented is_finished

--- WSI/zadanie3/main.py
+++ WSI/zadanie3/main.py
@@ -1,27 +1,25 @@
-import numpy as np
-
-
 class Player():
-    def move(self, board):
-        move_type = int(input('> '))
+    def move(self, board: list) -> str:
+        move_type = input('> ')
         return move_type
 
 
 class Oponent():
-    def move():
+    def move(self, board):
         pass
 
 
 class TicTacToe():
-    def __init__(self, dimension: int, players, who_starts: str):
+    def __init__(self, dimension: int, player1, player2):
         # who_starts = '0' - player starts
         # who_starts = '1' - oponent starts
 
         self.signs = ('o', 'x')
         self.dimension = dimension
-        self.players = players
-        self.who_moves = who_starts
+        self.players = (player1, player2)
+        self.who_moves = 0
         self.board = self.create_board(dimension)
+        self.finished = False
 
     def create_board(self, dimension: int):
         num = 1
@@ -29,17 +27,17 @@ class TicTacToe():
         for i in range(dimension):
             row = []
             for j in range(dimension):
-                row.append(num)
+                row.append(str(num))
                 num += 1
             table.append(row)
-        matrix = np.array(table)
-        return matrix
+        print(table)
+        return table
 
     def print_board(self):
         for i in range(self.dimension):
             fields = []
             for j in range(self.dimension):
-                fields.append(str(self.board[i][j]))
+                fields.append(self.board[i][j])
             row = '|' + '|'.join(fields) + '|'
             print(row)
 
@@ -47,27 +45,67 @@ class TicTacToe():
         player = self.players[self.who_moves]
         sign = self.signs[self.who_moves]
         move_type = player.move(self.board)
-        self.board[self.board == move_type] = sign
+
+        self.change_board(move_type, sign)
+        self.finished = self.is_finished(sign)
         self.who_moves = not self.who_moves
 
-    def finished(self):
-        pass
+    def change_board(self, move_type, sign):
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                if self.board[i][j] == move_type:
+                    self.board[i][j] = sign
+
+    def is_finished(self, sign) -> bool:
+        def is_row_line():
+            for i in range(self.dimension):
+                is_line = True
+                for j in range(self.dimension):
+                    if self.board[i][j] != sign:
+                        is_line = False
+                if is_line:
+                    return True
+
+        def is_col_line():
+            for i in range(self.dimension):
+                is_line = True
+                for j in range(self.dimension):
+                    if self.board[j][i] != sign:
+                        is_line = False
+                if is_line:
+                    return True
+
+        def is_diag_line():
+            is_line = True
+            for i in range(self.dimension):
+                if self.board[i][i] != sign:
+                    is_line = False
+            if is_line:
+                return True
+
+            is_line = True
+            for i in range(self.dimension):
+
+                if self.board[i][-i-1] != sign:
+                    is_line = False
+            if is_line:
+                return True
+        
+        return is_row_line() or is_col_line() or is_diag_line()
 
 
 def main():
-    players = (Player(), Oponent())
-    starting_player = int(input('Who starts? [0/1]\n'))
     game = TicTacToe(
         dimension=3,
-        players=players,
-        who_starts=starting_player,
+        player1=Player(),
+        player2=Oponent()
         )
     game.print_board()
 
-    while not game.finished():
+    while not game.finished:
         game.move()
         game.print_board()
-    pass
+    print('u won')
 
 
 if __name__ == "__main__":
