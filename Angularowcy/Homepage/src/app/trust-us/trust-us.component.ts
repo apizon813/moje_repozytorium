43c04b6081edc49ce50c5e6ccb55449ae9cce5af
apizon <aleksanderpizon@gmail.com>
2024-05-30 10:55:32 +0200

    zmieniono trust us

--- Angularowcy/Homepage/src/app/trust-us/trust-us.component.ts
+++ Angularowcy/Homepage/src/app/trust-us/trust-us.component.ts
@@ -1,31 +1,13 @@
-import { CommonModule, NgFor } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trust-us',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule],
   templateUrl: './trust-us.component.html',
   styleUrl: './trust-us.component.css'
 })
-export class TrustUsComponent implements OnInit, OnDestroy{
-  currentIndex: number = 0;
-  changeInterval: number = 3000;
-  intervalId: any;
-
-  ngOnInit(): void {
-    this.intervalId = setInterval(() => {
-      this.showNextItem();
-    }, this.changeInterval);
-  }
-
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-  showNextItem(): void {
-    this.currentIndex = (this.currentIndex + 1) % 3;
-  }
+export class TrustUsComponent {
+  hoveredIndex: number = -1;
 }
