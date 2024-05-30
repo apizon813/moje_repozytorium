5f0ece7c66cc935020d8a48465deea45362453c8
apizon <aleksanderpizon@gmail.com>
2024-05-30 10:37:14 +0200

    colors

--- Angularowcy/Homepage/src/app/app.component.ts
+++ Angularowcy/Homepage/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, NgFor],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, NgFor, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
